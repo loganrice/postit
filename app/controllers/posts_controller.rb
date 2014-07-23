cb24ce9a3461dba4b1775940a4be9370bf5759b5
loganrice <loganrice72@gmail.com>
2014-07-23 15:18:04 -0700

    forms w notes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,18 +8,44 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    binding.pry
+
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render :new
+    end
 
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
 
+    if @post.update(post_params)
+      flash[:notice] = "The post was updated"
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  private 
+  # strong paramaters
+  def post_params
+    #:post is the top level key
+    # params.require(:post).permit(:title, :url)
+    #params.require(:post).permit! works for everything
+    params.require(:post).permit!
   end
 end
