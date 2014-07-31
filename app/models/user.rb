4b263aaf9b2dbc8ac834f63d3d471eb4a7b72a1b
Logan Rice <loganrice72@gmail.com>
2014-07-31 12:58:50 -0700

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   has_many :comments
 
   validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create
+
   has_secure_password validations: false
   
 end
