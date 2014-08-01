49b68172075e83aaf5557c3c7026a3bf4e5f0382
loganrice <loganrice72@gmail.com>
2014-08-01 09:49:36 -0700

    updated user in main nav and user edit controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_up_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
@@ -24,8 +26,16 @@ class UsersController < ApplicationController
     set_up_user
   end
 
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Profile was updated."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
   def set_up_user
-    @user = User.find(session[:user_id])
+    @user = User.find(params[:id])
   end
 
   def user_params
