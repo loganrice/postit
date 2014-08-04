c472383804ee93ac8b6c7beb53d18ceb4518b105
loganrice <loganrice72@gmail.com>
2014-08-04 10:01:55 -0700

    votes table and setup polymorphic associations w post and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+  has_many :votes
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create
 
   has_secure_password validations: false
-  
 end
