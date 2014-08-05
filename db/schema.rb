e9e94243c65cd5d68de23d4478431a407996a0a0
Logan Rice <loganrice72@gmail.com>
2014-08-05 16:13:00 -0700

    post vote views

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,7 @@
 ActiveRecord::Schema.define(version: 20140804161611) do
 
   create_table "categories", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "name"
   end
 
   create_table "comments", force: true do |t|
