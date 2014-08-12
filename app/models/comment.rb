ab34a4b6a3de5c58ebee15e48994da4ce9257b87
Logan Rice <loganrice72@gmail.com>
2014-08-12 12:14:01 -0700

    added sluggable module

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   include VoteableLoganAug
+  
   belongs_to :user
   belongs_to :post
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include Sluggable
+
   has_many :posts
   has_many :comments
   has_many :votes
@@ -8,7 +10,7 @@ class User < ActiveRecord::Base
 
   has_secure_password validations: false
 
-  after_validation :generate_slug
+  sluggable_column :username
 
   def admin?
     self.role.to_s == 'admin'
@@ -18,34 +20,4 @@ class User < ActiveRecord::Base
     self.role == 'moderator?'
   end
 
-  def generate_slug
-    the_slug = to_slug(self.username)
-    user = User.find_by slug: the_slug
-    count = 2
-    while user && user != self
-      the_slug = append_suffix(the_slug, count)
-      user = User.find_by slug: the_slug
-      count += 1
-    end
-    self.slug = the_slug.downcase
-  end
-
-  def append_suffix(str, count)
-    if str.split('-').last.to_i != 0
-      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
-    else
-      return str + "-" + count.to_s
-    end
-  end
-
-  def to_slug(name)
-    str = name.strip
-    str.gsub!(/\s*[^A-Za-z0-9]\s*/, '-')
-    str.gsub!(/-+/, "-")
-    str.downcase
-  end
-
-  def to_param
-    self.slug
-  end
 end
